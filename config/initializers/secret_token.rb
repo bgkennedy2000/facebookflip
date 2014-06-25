ae585c319427c586cb20da4c1c8578a817cb46c4
Ben Kennedy <bgkennedy@gmail.com>
2014-06-25 09:05:48 +0100

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbFlip::Application.config.secret_token = 'cc82c7608eb5816d9fa090d81d255cd23ebc7202edcd72fee8e27d2bca0cf5d36b31d72a8abd9bbaf3510a676c0e3a170544fb5483ee948ef7bd279db77d3ada'
