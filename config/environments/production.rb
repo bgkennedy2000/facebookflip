15fd4e338e16e76ea13155b26bde7633fbd1d621
Ben Kennedy <bgkennedy@gmail.com>
2014-06-26 14:12:37 +0100

    debug logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,9 @@ FbFlip::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
+  # config.logger.level = Logger::DEBUG
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
